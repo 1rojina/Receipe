cc379564dee0073f78e504a9a692c2bbb024286d
1rojina <140293960+1rojina@users.noreply.github.com>
2023-09-14 23:22:59 +0545

    Add files via upload

--- rojina.js
+++ rojina.js
@@ -3,15 +3,13 @@ const recipeList = document.querySelector('#recipe-list');
 const noRecipes = document.getElementById('no-recipes');
 const searchBox = document.getElementById('search-box');
 
-// Define recipes array
 let recipes = [];
 
-// Handle form submit
+
 function handleSubmit(event) {
-  // Prevent default form submission behavior
+ 
   event.preventDefault();
 
-  // Get recipe name, ingredients, and method input values
   const nameInput = document.querySelector('#recipe-name');
   const ingrInput = document.querySelector('#recipe-ingredients');
   const methodInput = document.querySelector('#recipe-method');
@@ -19,28 +17,28 @@ function handleSubmit(event) {
   const ingredients = ingrInput.value.trim().split(',').map(i => i.trim());
   const method = methodInput.value.trim();
 
-  // Check if recipe name, ingredients, and method are valid
+ 
   if (name && ingredients.length > 0 && method) {
-    // Create new recipe object and add it to recipes array
+   
     const newRecipe = { name, ingredients, method };
     recipes.push(newRecipe);
 
-    // Clear form inputs
+   
     nameInput.value = '';
     ingrInput.value = '';
     methodInput.value = '';
 
-    // Add new recipe to recipe list
+    
     displayRecipes();
   }
 }
 
-// Display recipes in recipe list
+
 function displayRecipes() {
   recipeList.innerHTML = '';
   recipes.forEach((recipe, index) => {
     const recipeDiv = document.createElement('div');
-  // Create div to display the individual recipe, for each recipe
+  
     recipeDiv.innerHTML = `
       <h3>${recipe.name}</h3>
       <p><strong>Ingredients:</strong></p>
@@ -53,7 +51,7 @@ function displayRecipes() {
     recipeDiv.classList.add('recipe');
     recipeList.appendChild(recipeDiv);
   });
-  // Display warning when there are no recipes in the list
+  
   if (recipes.length > 0) {
   noRecipes.style.display = 'none';
   }
@@ -62,7 +60,6 @@ function displayRecipes() {
   }
 }
 
-// Handle recipe deletion
 function handleDelete(event) {
   if (event.target.classList.contains('delete-button')) {
     const index = event.target.dataset.index;
@@ -72,7 +69,7 @@ function handleDelete(event) {
   }
 }
 
-// Search recipes by search query
+
 function search(query) {
   const filteredRecipes = recipes.filter(recipe => {
     return recipe.name.toLowerCase().includes(query.toLowerCase());
@@ -96,7 +93,7 @@ function search(query) {
   });
 }
 
-// Add event listeners
+
 form.addEventListener('submit', handleSubmit);
 recipeList.addEventListener('click', handleDelete);
 searchBox.addEventListener('input', event => search(event.target.value));
